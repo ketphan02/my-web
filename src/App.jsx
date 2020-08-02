ec003cbad16cc00ca9102fe39e98a257ae8d941a
phankiet02 <tuankietvn@outlook.com>
2020-08-02 19:14:40 +0700

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,41 +2,55 @@ import React from 'react';
 import './App.css';
 import './Header.css';
 import './Font.css';
+import { Switch } from 'react-router-dom';
+import About from './About';
 
 export default App;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-          <content-area>
-            <h2>
-              <a href="/my-web/" className="title" > PHAN_KIET_BLOG </a>
-            </h2>
-            <form action="https://google.com">
-              <button className="title-button"> MAGIC BUTTON </button>
-            </form>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+            <content-area>
+              <h2>
+                <a href="/my-web/" className="title" > PHAN_KIET_BLOG </a>
+              </h2>
+              <form action="https://google.com">
+                <button className="title-button"> MAGIC BUTTON </button>
+              </form>
 
-          </content-area>
+            </content-area>
 
-          <content-run>
-              <run-txt>
-                PhanKiet blog web
-              </run-txt>
-          </content-run>
-      
-      <body-content>
-        <intro>
-          <h3> HELLO, IT'S PHAN KIET </h3>
-          <p>This is my website which allows me to post my projects, my experience about coding, ML, DL, gym, stuff, etc. Want to know more ? "Almost everything" about me is in <a href="/my-web/about.html"> HERE</a>.
-          <br></br>
-          Okay, here we go.
-          </p>
-        </intro>
-      </body-content>
-      
-      </header>
-    </div>
+            <content-run>
+                <run-txt>
+                  PhanKiet blog web
+                </run-txt>
+            </content-run>
+        
+        <body-content>
+          <intro>
+            <h3> HELLO, IT'S PHAN KIET </h3>
+            <p>This is my website which allows me to post my projects, my experience about coding, ML, DL, gym, stuff, etc. Want to know more ? "Almost everything" about me is in <a href="/my-web/about.html"> HERE</a>.
+            <br></br>
+            Okay, here we go.
+            </p>
+          </intro>
+        </body-content>
+        
+        </header>
+      </div>
+
+      <Switch>
+
+        <Route path="/about">
+          <About />
+        </Route>
+
+
+      </Switch>
+
+    </Router>
   );
 }
 
