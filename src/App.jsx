d39e161e16e82c99e5911c5280d2020a3f55e072
phankiet02 <tuankietvn@outlook.com>
2020-08-02 10:52:28 +0700

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './Header.css';
+import './Font.css';
 
 export default App;
 
@@ -8,20 +10,35 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-
           <content-area>
-
             <h2 className="title">
               PHAN_KIET_BLOG
             </h2>
-
             <button className="title-button">
               MAGIC BUTTON
             </button>
 
-          </content-area>  
+          </content-area>
+
+          <content-run>
+
+            <run-txt>
+                <a href="https://google.com">
+                  PHANKIET blog. Let's see what will be put in.
+                </a>
+            </run-txt>
+          </content-run>  
+
       </header>
 
+
+      <bd>
+
+
+        <h1> HELLO, IT'S PHAN KIET</h1>
+
+
+      </bd>
     </div>
   );
 }
